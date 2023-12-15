a98fa09affae37cc3608328ef27055763e7c2cfe
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-12-16 04:22:16 +0700

    Update GlossaryController.java

--- src/main/java/com/example/controller/GlossaryController.java
+++ src/main/java/com/example/controller/GlossaryController.java
@@ -12,7 +12,7 @@ import com.example.model.GlossaryPagination;
 @RequestMapping("/glossary")
 public class GlossaryController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="http://localhost:8082";
+	private String url="https://server2-production-29ab.up.railway.app";
 	//private String url="https://da-server2-production.up.railway.app";
 	@GetMapping("/get")
 	private String getAll(Model model, @RequestParam(name="pageNum", defaultValue = "1", required = false) int pageNum,
